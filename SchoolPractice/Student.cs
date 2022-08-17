bdb4945da0daab626ae35c194605cbdc13b94be3
Erica <johnson.ericastarr@gmail.com>
2022-08-17 17:37:03 -0500

    initial

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -3,13 +3,30 @@
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
 
+
 namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; } = 0;
+        public double Gpa { get; set; } = 0.0;
+
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int studentId) : this(name, studentId, 0, 0) { }
+
+        public Student(string name) : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
     }
 }
